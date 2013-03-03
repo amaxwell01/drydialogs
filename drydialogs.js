620f11f64c4fe7c631c822053911e1e6f7d2a272
Andrew Maxwell <amaxwell01@gmail.com>
2013-03-03 07:40:25 -0800

    Update drydialogs.js
    Updated the position method to support with <= windowWidth vs. === windowWidth

--- drydialogs.js
+++ drydialogs.js
@@ -26,7 +26,7 @@ var dialog = {
         var dialogClass = '';
         if( args.dialogClass )
         {
-            dialogClass = 'class="' + args.dialogClass + '"';
+            dialogClass = 'class="customDialog ' + args.dialogClass + '"';
         }
         
         var dataAttributes = '';
@@ -57,7 +57,7 @@ var dialog = {
             'CustomDialogHeader"' + headerClass + '><h2>' + args.title + '</h2>' +
             '<div id="customDialogClose"onclick="dialog.closeDialog(\'' +name + '\');">x</div></div>';
         } else {
-            titleBody = '<div id="customDialogClose"onclick="dialog.closeDialog(\'' +name + '\');">x</div>';
+            titleBody = '<div id="customDialogClose" onclick="dialog.closeDialog(\'' +name + '\');">x</div>';
         }
         
         if( args.textBody ) {
@@ -75,99 +75,92 @@ var dialog = {
         else {
             bodyClass = 'customDialogBody';
         }
-		
-		if( args.buttons ) {
-			var buttons = '';
+        
+        if( args.buttons ) {
+            var buttons = '';
 
-			for( var i = 0; i < args.buttons.length; i++ ) {
-				var disabled = '';
-				var buttonClass = '';
-				if( args.buttons[i].disabled ) {
-					disabled = 'disabled="disabled"';
-					buttonClass += 'disabled ';
-				}
-				
-				if( typeof(customDialogButtons) == 'undefined' ) {
-					customDialogButtons = {};
-				}
-				
-				if( args.buttons[i].callback ) {
-					delete customDialogButtons[i + name];
-					customDialogButtons[i + name] = args.buttons[i].callback;
-				}
-			
-				var buttonFunction;
-				var parameters = args.buttons[i].parameters ? args.buttons[i].parameters : '';
-				var eventType = args.buttons[i].eventType ? args.buttons[i].eventType : 'onclick';
-				var buttonID = args.buttons[i].id ? 'id="' + args.buttons[i].id + '" ' : '';
-				
-				// TODO - If there is no button title, use the button value
-				var buttonTitle = args.buttons[i].title ? 'title="' + args.buttons[i].title + '" ' : '';
-				buttonClass += args.buttons[i].buttonClass ? args.buttons[i].buttonClass : 'customDialogButton';
-				
-				
-				
-				buttons += '<input type="button" ' + disabled + ' name="' + args.buttons[i].name + '" class="' + buttonClass + '" ' + buttonID + eventType + '="' + 'customDialogButtons[\'' + i +name + '\'](' + parameters + ')' + '" value="' + args.buttons[i].value + '"/>';
-			}
-		}
-		
-		var buttonContainerClass;
-		if( typeof(args.buttonParameters) == 'undefined' ) {
-			args.buttonParameters = {};
-		}
-		
-		if( args.buttonParameters.containerClass ) {
-			buttonContainerClass = args.buttonParameters.containerClass;
-		}
-		else {
-			buttonContainerClass = 'customDialogButtonContainer';
-		}
-		
-		var buttonElement = '';
-		if( typeof(args.buttons) != 'undefined' ) {
-			buttonElement = '<div id="' + name + 'DialogButtons" class="' + buttonContainerClass +'">' + buttons + '</div>';
-		}
-		
-		// create a empty array and use it as a way to store a long string/variable which contains the custom dialog
-		var dialogBody = new Array(7);
-		dialogBody[0] = '<div id="' + name + 'DialogWrapper">';
-		dialogBody[1] = '<div id="' + name + 'CustomDialog" ' + dialogClass + ' ' + dataAttributes + ' >';
-		dialogBody[2] = titleBody;
+            for( var i = 0; i < args.buttons.length; i++ ) {
+                var disabled = '';
+                var buttonClass = '';
+                if( args.buttons[i].disabled ) {
+                    disabled = 'disabled="disabled"';
+                    buttonClass += 'disabled ';
+                }
+                
+                if( typeof(customDialogButtons) == 'undefined' ) {
+                    customDialogButtons = {};
+                }
+                
+                if( args.buttons[i].callback ) {
+                    delete customDialogButtons[i + name];
+                    customDialogButtons[i + name] = args.buttons[i].callback;
+                }
+            
+                var buttonFunction;
+                var parameters = args.buttons[i].parameters ? args.buttons[i].parameters : '';
+                var eventType = args.buttons[i].eventType ? args.buttons[i].eventType : 'onclick';
+                var buttonID = args.buttons[i].id ? 'id="' + args.buttons[i].id + '" ' : '';
+                
+                // TODO - If there is no button title, use the button value
+                var buttonTitle = args.buttons[i].title ? 'title="' + args.buttons[i].title + '" ' : '';
+                buttonClass += args.buttons[i].buttonClass ? args.buttons[i].buttonClass : 'customDialogButton';
+                
+                
+                
+                buttons += '<input type="button" ' + disabled + ' name="' + args.buttons[i].name + '" class="' + buttonClass + '" ' + buttonID + eventType + '="' + 'customDialogButtons[\'' + i +name + '\'](' + parameters + ')' + '" value="' + args.buttons[i].value + '"/>';
+            }
+        }
+        
+        var buttonContainerClass;
+        if( typeof(args.buttonParameters) == 'undefined' ) {
+            args.buttonParameters = {};
+        }
+        
+        if( args.buttonParameters.containerClass ) {
+            buttonContainerClass = args.buttonParameters.containerClass;
+        }
+        else {
+            buttonContainerClass = 'customDialogButtonContainer';
+        }
+        
+        var buttonElement = '';
+        if( typeof(args.buttons) != 'undefined' ) {
+            buttonElement = '<div id="' + name + 'DialogButtons" class="' + buttonContainerClass +'">' + buttons + '</div>';
+        }
+        
+        // create a empty array and use it as a way to store a long string/variable which contains the custom dialog
+        var dialogBody = new Array(7);
+        dialogBody[0] = '<div id="' + name + 'DialogWrapper">';
+        dialogBody[1] = '<div id="' + name + 'CustomDialog" ' + dialogClass + ' ' + dataAttributes + ' data-name="' + name + '">';
+        dialogBody[2] = titleBody;
         dialogBody[3] = '<div id="' + name + 'CustomDialogBody" class="' + bodyClass + '">' + body + '</div>';
         dialogBody[4] = buttonElement;
         dialogBody[5] = '</div>';
         dialogBody[6] = '</div>';
-		$('body').append( dialogBody.join('') );
-		
-		// SET THE STYLING FOR THE CUSTOM DIALOG
-		if( args.bodyClass ) {
-			$('#' + name + 'CustomDialogBody').addClass( args.customDialogBodyClass );
-		}
-		else {
-			$('#' + name + 'CustomDialogBody').addClass( 'customDialogBody' );
-		}
-		
-		if( args.width ) {
-			$('#' + name + 'CustomDialog').css( 'width', args.width );
-		}
-		else {
-			$('#' + name + 'CustomDialog').css( 'width', '400px' );
-		}
+        $('body').append( dialogBody.join('') );
+        
+        // SET THE STYLING FOR THE CUSTOM DIALOG
+        if( args.bodyClass ) {
+            $('#' + name + 'CustomDialogBody').addClass( args.customDialogBodyClass );
+        }
+        else {
+            $('#' + name + 'CustomDialogBody').addClass( 'customDialogBody' );
+        }
         
         if( args.height ) {
-			$('#' + name + 'CustomDialog').css( 'height', args.height );
-		}
+            $('#' + name + 'CustomDialog').css( 'height', args.height );
+        }
         
         if( args.maxHeight ) {
-			$('#' + name + 'CustomDialog').css( 'max-height', args.maxHeight );
-		}
-		
-		if( args.position ) {
-			$('#' + name + 'CustomDialog').css( 'position', args.position );
-		}
-		else {
-			$('#' + name + 'CustomDialog').css( 'position', 'absolute' );
-		}
+            $('#' + name + 'CustomDialog').css( 'max-height', args.maxHeight );
+        }
+        
+        if( args.position ) {
+            $('#' + name + 'CustomDialog').css( 'position', args.position );
+        }
+        else {
+            $('#' + name + 'CustomDialog').css( 'position', 'absolute' );
+        }
         
         if( args.backgroundColor ) {
             $('#' + name + 'DialogWrapper').css('background-color', args.backgroundColor );
@@ -178,8 +171,8 @@ var dialog = {
             // needs to be RGBA since opacity will make all children have the opacity and not just the parent
             //$('#' + name + 'DialogWrapper');
         }
-		
-		dialog.position( args );
+        
+        dialog.position( args );
         
         
         if( args.initialLocation) {
@@ -188,45 +181,78 @@ var dialog = {
             // this can be the center(default) or from the trigger location
             // 'center', // optional, default is center, but it can be 'trigger'
         }        
-		
-		if( args.success ) {
-			args.success();
-		}
-	},
-	
-	position : function( args ) {
-		var name = '';
-		if( args && args.name ) {
-			name = args.name;
-		}
-	
-        if( args && args.top ) {
-			customDialogTop = args.top;
-		}
-		else {
-			var windowHeightHalf = window.innerHeight / 2;
-			var customDialogHeight = $('#' + name + 'CustomDialog').height();
-			var customDialogHeightHalf = customDialogHeight / 2;
-			customDialogTop = windowHeightHalf - customDialogHeightHalf;
-			
-			if( customDialogTop < 0 ) {
-				customDialogTop = 50;
-			}
-		}
         
-		var customDialogLeft;
-		if( args && args.left ) {
-			customDialogLeft = args.left;
-		}
-		else {
-			var windowWidthHalf = window.innerWidth / 2,
-            customDialogWidth = $('#' + name + 'CustomDialog').width(),
-            customDialogWidthHalf = customDialogWidth / 2,
+        if( args.success ) {
+            args.success();
+        }
+
+        if ( args.responsive ) {
+            dialog.responsive();
+        }
+    },
+    
+    position : function( args ) {
+        var name = '';
+        if( args && args.name ) {
+            name = args.name;
+        }
+    
+        if( args && args.top ) {
+            customDialogTop = args.top;
+        }
+        else {
+            var windowHeightHalf = window.innerHeight / 2;
+            var customDialogHeight = $('#' + name + 'CustomDialog').height();
+            var customDialogHeightHalf = customDialogHeight / 2;
+            customDialogTop = windowHeightHalf - customDialogHeightHalf;
             
-            customDialogLeft = windowWidthHalf - customDialogWidthHalf;
-		}
-		
-		$('#' + name + 'CustomDialog').css( 'left', customDialogLeft );
-		$('#' + name + 'CustomDialog').css( 'top', customDialogTop );
-	}
+            if( customDialogTop < 0 ) {
+                customDialogTop = 50;
+            }
+        }
+        
+        var customDialogLeft;
+        if( args && args.left ) {
+            customDialogLeft = args.left;
+        }
+        else {
+            var windowWidth = $(window).width();
+            var windowWidthHalf = windowWidth / 2;
+            var customDialogWidth = $('#' + name + 'CustomDialog').outerWidth();
+            var customDialogWidthHalf = customDialogWidth / 2;
+
+            if ( customDialogWidth <= windowWidth ) {
+                customDialogLeft = 0;
+            } else {
+                customDialogLeft = windowWidthHalf - customDialogWidthHalf;
+            }
+        }
+
+        $('#' + name + 'CustomDialog').css( 'left', customDialogLeft );
+        $('#' + name + 'CustomDialog').css( 'top', customDialogTop );
+    },
+
+    responsive: function() {
+
+        var updatePosition = function() {
+            var dialogs = $('.customDialog');
+            var name;
+
+            $.each( dialogs, function( key, value ) {
+                name = $(value).attr('data-name');
+                dialog.position({ name: name });
+            });
+        };
+
+        // Center the dialog when resizing
+        $(window).resize( function() {
+            if( timer ) {
+                clearTimeout(timer);
+            }
+         
+            var timer = setTimeout( function() {
+                updatePosition();
+            }, 100 );    
+        });
+    }
 }
